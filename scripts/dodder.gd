a1b4ebaae75bb470466f23ae49bc4581e59ecd1b
Jan Hrastnik <jan.hrastnik2@gmail.com>
2024-03-09 21:23:07 +0100

    first commit

---
+++ scripts/dodder.gd
@@ -0,0 +1,31 @@
+class_name Dodder
+
+extends StaticBody2D
+
+@onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func move_to(click_pos: Vector2):
+	var diff = click_pos - position
+	var diff_normed = diff.normalized()
+	var dist = diff.length()
+	var steps = int(dist / 32)
+	var intermediary_points = []
+	for i in range(1, steps+1):
+		intermediary_points.append(position + diff_normed*32*i)
+	intermediary_points.append(click_pos)
+	while !intermediary_points.is_empty():
+		var point = intermediary_points.pop_front()
+		animation_player.play("shrink")
+		await get_tree().create_timer(0.3).timeout
+		position = point
+		animation_player.play("grow")
+		await get_tree().create_timer(0.3).timeout
---
+++ scripts/map.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+const WIDTH = 128
+const HEIGHT = 128
+
+@onready var hivemind: HivemindSingleton = get_node("/root/Hivemind")
+var dodder: Dodder = null
+
+func _ready():
+	spawn_plant_on_random_point("basic_bush")
+	spawn_baby_dodder()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		if dodder:
+			#print(event.position)
+			dodder.move_to(get_local_mouse_position())
+
+func spawn_plant_on_random_point(plant_name: String):
+	var plant = load("res://scenes/plants/{name}.tscn".format({name=plant_name}))
+	var plant_instance: StaticBody2D = plant.instantiate()
+	
+	var rng = RandomNumberGenerator.new()
+	var x = rng.randi_range(0, WIDTH)
+	var y = rng.randi_range(0, HEIGHT)
+	
+	plant_instance.translate(Vector2(x,y))
+	
+	add_child(plant_instance)
+
+func spawn_baby_dodder():
+	var dodder_scene = load("res://scenes/dodder/dodder.tscn")
+	var dodder_instance: Dodder = dodder_scene.instantiate()
+	
+	dodder_instance.position = Vector2(WIDTH/2,HEIGHT/2)
+	dodder = dodder_instance
+	
+	add_child(dodder_instance)
