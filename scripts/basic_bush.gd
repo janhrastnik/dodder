916fbb1a9014e8bf7e7f8054e50ba9cb88357912
Jan Hrastnik <jan.hrastnik2@gmail.com>
2024-03-12 21:44:51 +0100

    plant generic class working

--- scripts/basic_bush.gd
+++ scripts/basic_bush.gd
@@ -6,77 +6,8 @@ extends Plant
 ## Na plants se lahko attacha dodder in potem od njih pobira stvari,
 ## kot so nutrients in dna (abilities).
 
-#@onready var attach_label: Label = get_node("Attach Label")
 @onready var sprite: AnimatedSprite2D = get_node("Sprite")
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
-@onready var label_animation_player: AnimationPlayer = get_node("LabelAnimationPlayer")
-@onready var quicktime_timer : Timer = get_node("QuicktimeTimer")
-@onready var quicktime_start_timer : Timer = get_node("QuicktimeStartTimer")
-@onready var qte_display : HBoxContainer = get_node("QTE Container")
-@onready var shake_sound: AudioStreamPlayer = get_node("ShakeSound")
-@onready var harvest_sound: AudioStreamPlayer = get_node("HarvestSound")
-@onready var qte_indicator_sound: AudioStreamPlayer = get_node("QTEindicatorSound")
-
-# qte številka ki jo player mora pritisnit
-var qte_prompt = null
-
-# ko reachamo 0, se začne qte
-var qte_countdown = null
-
-var rng = RandomNumberGenerator.new()
-
-func _input(event):
-	if ((event.is_action_pressed("QTE1") and qte_prompt == 1) \
-	or (event.is_action_pressed("QTE2") and qte_prompt == 2) \
-	or (event.is_action_pressed("QTE3") and qte_prompt == 3)) \
-	and not quicktime_timer.is_stopped():
-		qte_success()
-
-func qte_success():
-	harvest_sound.play()
-	dodder.change_nutrients(1)
-	qte_stop()
-	show_harvest()
-
-func qte_start():
-	qte_indicator_sound.play()
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	var num = rng.randi_range(0, 2)
-	
-	var sprite: AnimatedSprite2D = null
-	if num == 0:
-		sprite = qte_display.get_node("One/One Button")
-		qte_prompt = 1
-	elif num == 1:
-		sprite = qte_display.get_node("Two/Two Button")
-		qte_prompt = 2
-	else:
-		sprite = qte_display.get_node("Three/Three Button")
-		qte_prompt = 3
-	sprite.animation = "active"
-
-	quicktime_timer.start()
-
-func qte_stop():
-	quicktime_timer.stop()
-	qte_prompt = null
-	
-	var one: AnimatedSprite2D = qte_display.get_node("One/One Button")
-	one.set_frame(0)
-	one.animation = "idle"
-	var two: AnimatedSprite2D = qte_display.get_node("Two/Two Button")
-	two.set_frame(0)
-	two.animation = "idle"
-	var three: AnimatedSprite2D = qte_display.get_node("Three/Three Button")
-	three.set_frame(0)
-	three.animation = "idle"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	super()
-	#attach_label.text = "{plant_name}\nAttach [e]".format({plant_name=plant_name})
-	rng.randomize()
 
 func _on_body_entered(body):
 	if body is Dodder:
@@ -90,34 +21,14 @@ func _on_body_exited(body):
 ## Predvaja animacijo, ko se dodder attacha gor na plant.
 func attach(d : Dodder):
 	super(d)
-	dodder = d
-	qte_display.visible = true
-	quicktime_start_timer.start()
 	
 	animation_player.play("shake")
-	shake_sound.play()
 	await get_tree().create_timer(0.1).timeout
 	sprite.animation = "infected"
 
 func detach():
-	qte_stop() # If in qte
-	quicktime_start_timer.stop()
-	qte_display.visible = false
+	super()
 	
 	animation_player.play("shake")
-	shake_sound.play()
 	await get_tree().create_timer(0.1).timeout
 	sprite.animation = "idle"
-
-func _on_quicktime_timer_timeout():
-	qte_stop()
-
-func _on_quicktime_start_timer_timeout():
-	if qte_countdown == null:
-		qte_countdown = rng.randi_range(1, 5)
-	if dodder != null and quicktime_timer.is_stopped():
-		if qte_countdown <= 0:
-			qte_start()
-			qte_countdown = rng.randi_range(1, 5)
-		else:
-			qte_countdown -= 1
--- scripts/plant.gd
+++ scripts/plant.gd
@@ -12,10 +12,26 @@ var nutrients : int
 # needed nodes
 var popup_text: Node2D = null
 var qte_timer: Timer = null
+var start_timer: Timer = null
+var qte_display : HBoxContainer = null
+
+var shake_sound: AudioStreamPlayer = null
+var harvest_sound: AudioStreamPlayer = null
+var qte_indicator_sound: AudioStreamPlayer = null
+
+# qte številka ki jo player mora pritisnit
+var qte_prompt = null
+
+# ko reachamo 0, se začne qte
+var qte_countdown = null
+
+var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	plant_name = stats.name
 	nutrients = stats.nutrients
+	
+	rng.randomize()
 	
 	# construct subnodes
 	var popup_text_packed = load("res://scenes/ui/popup_text.tscn")
@@ -26,8 +42,44 @@ func _ready() -> void:
 	qte_timer = Timer.new()
 	qte_timer.one_shot = true
 	qte_timer.name = "QTE Timer"
-	add_child(popup_text)
+	#qte_timer.connect("timeout", qte_timer_timeout)
+	add_child(qte_timer)
+
+	start_timer = Timer.new()
+	start_timer.name = "Start Timer"
+	#qte_timer.connect("timeout", qte_timer_timeout)
+	add_child(start_timer)
 	
+	var qte_display_packed = load("res://scenes/plants/qte_container.tscn")
+	qte_display = qte_display_packed.instantiate()
+	add_child(qte_display)
+	
+	# add sounds
+	shake_sound = AudioStreamPlayer.new()
+	shake_sound.stream = load("res://audio/sfx/Bush Shake 2.wav")
+	shake_sound.volume_db = -10
+	add_child(shake_sound)
+
+	harvest_sound = AudioStreamPlayer.new()
+	harvest_sound.stream = load("res://audio/sfx/Nutrient.wav")
+	harvest_sound.volume_db = -7
+	add_child(harvest_sound)
+
+	qte_indicator_sound = AudioStreamPlayer.new()
+	qte_indicator_sound.stream = load("res://audio/sfx/qte_indicator.wav")
+	qte_indicator_sound.volume_db = -7
+	add_child(qte_indicator_sound)
+	
+	qte_timer.connect("timeout", on_quicktime_timer_timeout)
+	start_timer.connect("timeout", on_start_timer_timeout)
+
+func _input(event):
+	if ((event.is_action_pressed("QTE1") and qte_prompt == 1) \
+	or (event.is_action_pressed("QTE2") and qte_prompt == 2) \
+	or (event.is_action_pressed("QTE3") and qte_prompt == 3)) \
+	and not qte_timer.is_stopped():
+		qte_success()
+
 func show_popup_text():
 	popup_text.show()
 	popup_text.get_node("AnimationPlayer").play("text_popup")
@@ -47,7 +99,67 @@ func show_harvest():
 	popup_text.get_node("Harvest Label").hide()
 
 func attach(d : Dodder) -> void:
-	pass
+	dodder = d
+	qte_display.visible = true
+	start_timer.start()
+	shake_sound.play()
 	
 func detach() -> void:
+	qte_stop()
+	start_timer.stop()
+	qte_display.visible = false
+	shake_sound.play()
+
+func qte_timer_timeout():
 	pass
+
+func qte_start():
+	qte_indicator_sound.play()
+	var num = rng.randi_range(0, 2)
+	
+	var button_sprite: AnimatedSprite2D = null
+	if num == 0:
+		button_sprite = qte_display.get_node("One/One Button")
+		qte_prompt = 1
+	elif num == 1:
+		button_sprite = qte_display.get_node("Two/Two Button")
+		qte_prompt = 2
+	else:
+		button_sprite = qte_display.get_node("Three/Three Button")
+		qte_prompt = 3
+	button_sprite.animation = "active"
+
+	qte_timer.start()
+
+func qte_stop():
+	qte_timer.stop()
+	qte_prompt = null
+	
+	var one: AnimatedSprite2D = qte_display.get_node("One/One Button")
+	one.set_frame(0)
+	one.animation = "idle"
+	var two: AnimatedSprite2D = qte_display.get_node("Two/Two Button")
+	two.set_frame(0)
+	two.animation = "idle"
+	var three: AnimatedSprite2D = qte_display.get_node("Three/Three Button")
+	three.set_frame(0)
+	three.animation = "idle"
+
+func qte_success():
+	harvest_sound.play()
+	dodder.change_nutrients(1)
+	qte_stop()
+	show_harvest()
+
+func on_quicktime_timer_timeout():
+	qte_stop()
+
+func on_start_timer_timeout():
+	if qte_countdown == null:
+		qte_countdown = rng.randi_range(1, 5)
+	if dodder != null and qte_timer.is_stopped():
+		if qte_countdown <= 0:
+			qte_start()
+			qte_countdown = rng.randi_range(1, 5)
+		else:
+			qte_countdown -= 1
