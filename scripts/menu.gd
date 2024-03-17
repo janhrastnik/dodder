2373dcd1986dea3f4b456be3815c7549b37a70c9
Jan Hrastnik <jan.hrastnik2@gmail.com>
2024-03-17 13:33:49 +0100

    menu wip

---
+++ scripts/menu.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_how_to_play_pressed():
+	pass # Replace with function body.
+
+func _on_button_pressed():
+	var map = load("res://scenes/map.tscn")
+	get_tree().change_scene_to_packed(map)
